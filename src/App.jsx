5a653ae9ff036c48483801f2aec8caa02227e95f
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-09 10:36:31 +0600

    add coffee form done

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='max-w-[1200px] mx-auto'>
-      <Link to="/add_coffee"><button className='my-btn'>Add Coffee</button></Link>
+    <div className='max-w-[1200px] mx-auto mt-28'>
+      <Link to="/add_coffee"><button className='my-btn mb-10'>Add Coffee</button></Link>
+      
+
     </div>
   );
 };
--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,11 +1,149 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AddCoffee = () => {
-    return (
-        <div>
-            add new coffee
+    const handleFormData = (e)=>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const teste = form.teste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photoUrl = form.photoUrl.value;
+        const coffee = {name, chef, supplier, texte: teste, category, details, photoUrl};
+        console.log(coffee)
+    }
+  return (
+    <div className="max-w-[1200px] mx-auto mt-36">
+      <Link to="/">
+        <button className="my-btn mb-10">Back Home</button>
+      </Link>
+      <form onSubmit={handleFormData}>
+        <div className="grid gap-6 mb-6 md:grid-cols-2 ">
+          <div>
+            <label
+              htmlFor="first_name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="first_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Coffee name"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="last_name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Chef
+            </label>
+            <input
+              type="text"
+              name="chef"
+              id="last_name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter coffee chef name"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="company"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Supplier
+            </label>
+            <input
+              type="text"
+              name="supplier"
+              id="company"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Coffee provider"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="phone"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Teste
+            </label>
+            <input
+              type="text"
+              name="teste"
+              id="phone"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter coffee teste"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="website"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Category
+            </label>
+            <input
+              type="text"
+              name="category"
+              id="website"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Coffee category"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="visitors"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Details
+            </label>
+            <input
+              type="text"
+              name="details"
+              id="visitors"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter coffee details"
+              required
+            />
+          </div>
         </div>
-    );
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Photo URL
+          </label>
+          <input
+            type="text"
+            name="photoUrl"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Photo URL"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
